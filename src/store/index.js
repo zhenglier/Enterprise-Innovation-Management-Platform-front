a0ef64fdd739c0c669db62ddeb54f4c6622c202a
zhang.zhe.114514 <gerald.rwx@qq.com>
2024-07-03 15:11:22 +0800

    zz roger

--- src/store/index.js
+++ src/store/index.js
@@ -1,19 +1,20 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-import getters from './getters'
-import app from './modules/app'
-import settings from './modules/settings'
-import user from './modules/user'
+import Vue from "vue";
+import Vuex from "vuex";
+import getters from "./getters";
+import app from "./modules/app";
+import settings from "./modules/settings";
+import user from "./modules/user";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
+//这里是分的模块
 const store = new Vuex.Store({
   modules: {
     app,
     settings,
-    user
+    user,
   },
-  getters
-})
+  getters,
+});
 
-export default store
+export default store;
