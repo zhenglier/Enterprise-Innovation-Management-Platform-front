5d7e1e4a8adfbcd96c64dc7fcd162d07446aea9e
gamer <1846579517@qq.com>
2024-07-10 23:05:18 +0800

    政策兑现界面的跳转

--- src/views/policy_related/realize/index.vue
+++ src/views/policy_related/realize/index.vue
@@ -155,7 +155,7 @@
           ></el-table-column>
           <el-table-column label="操作" width="120">
             <template slot-scope="scope">
-              <el-button type="text" @click="checkDetail(project.name)"
+              <el-button type="text" @click="checkDetail(scope.row.name)"
                 >查看详情</el-button
               >
             </template>
