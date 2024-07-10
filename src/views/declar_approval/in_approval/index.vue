afa850fc25a1480d1103adb74a12ca779a3757fb
gamer <1846579517@qq.com>
2024-07-10 11:43:07 +0800

    完善了入驻审核和搬离审核的分页组件

--- src/views/declar_approval/in_approval/index.vue
+++ src/views/declar_approval/in_approval/index.vue
@@ -4,18 +4,27 @@
       <div class="first-container">
         <el-alert :closable="false">未审核企业列表</el-alert>
         <el-table
-          :data="firsttableData"
+          :data="getCurrentPageData(firsttableData, currentPage1, pageSize1)"
           stripe
           style="width: 100%"
           class="first-table"
         >
-          <el-table-column prop="index" label="序号" width="180">
-          </el-table-column>
-          <el-table-column prop="unicode" label="社会统一代码" width="180">
-          </el-table-column>
-          <el-table-column prop="name" label="企业名称"> </el-table-column>
-          <el-table-column prop="date" label="申请时间" width="180">
-          </el-table-column>
+          <el-table-column
+            prop="index"
+            label="序号"
+            width="180"
+          ></el-table-column>
+          <el-table-column
+            prop="unicode"
+            label="社会统一代码"
+            width="180"
+          ></el-table-column>
+          <el-table-column prop="name" label="企业名称"></el-table-column>
+          <el-table-column
+            prop="date"
+            label="申请时间"
+            width="180"
+          ></el-table-column>
           <el-table-column label="企业情况" width="180">
             <template slot-scope="scope">
               <el-button
@@ -54,12 +63,11 @@
             @size-change="handleSizeChange"
             @current-change="handleCurrentChange"
             :current-page="currentPage1"
-            :page-sizes="[100, 200, 300, 400]"
-            :page-size="100"
+            :page-sizes="[1, 20, 50, 100]"
+            :page-size="pageSize1"
             layout="total, sizes, prev, pager, next, jumper"
-            :total="400"
-          >
-          </el-pagination>
+            :total="firsttableData.length"
+          ></el-pagination>
         </div>
       </div>
     </el-tab-pane>
@@ -67,18 +75,27 @@
       <div class="second-container">
         <el-alert :closable="false">已审核企业列表</el-alert>
         <el-table
-          :data="secondtableData"
+          :data="getCurrentPageData(secondtableData, currentPage2, pageSize2)"
           stripe
           style="width: 100%"
           class="second-table"
         >
-          <el-table-column prop="index" label="序号" width="180">
-          </el-table-column>
-          <el-table-column prop="unicode" label="社会统一代码" width="180">
-          </el-table-column>
-          <el-table-column prop="name" label="企业名称"> </el-table-column>
-          <el-table-column prop="date" label="申请时间" width="180">
-          </el-table-column>
+          <el-table-column
+            prop="index"
+            label="序号"
+            width="180"
+          ></el-table-column>
+          <el-table-column
+            prop="unicode"
+            label="社会统一代码"
+            width="180"
+          ></el-table-column>
+          <el-table-column prop="name" label="企业名称"></el-table-column>
+          <el-table-column
+            prop="date"
+            label="申请时间"
+            width="180"
+          ></el-table-column>
           <el-table-column label="企业情况" width="180">
             <template slot-scope="scope">
               <el-button
@@ -89,20 +106,22 @@
               >
             </template>
           </el-table-column>
-          <el-table-column prop="apply_condition" label="申请情况" width="180">
-          </el-table-column>
+          <el-table-column
+            prop="apply_condition"
+            label="申请情况"
+            width="180"
+          ></el-table-column>
         </el-table>
         <div class="page-part">
           <el-pagination
             @size-change="handleSizeChange"
             @current-change="handleCurrentChange"
             :current-page="currentPage2"
-            :page-sizes="[100, 200, 300, 400]"
-            :page-size="100"
+            :page-sizes="[1, 20, 50, 100]"
+            :page-size="pageSize2"
             layout="total, sizes, prev, pager, next, jumper"
-            :total="400"
-          >
-          </el-pagination>
+            :total="secondtableData.length"
+          ></el-pagination>
         </div>
       </div>
     </el-tab-pane>
@@ -113,9 +132,7 @@
 export default {
   data() {
     return {
-      //标签页跳转数据
       activeName: "second",
-      // 未完成审核列表
       firsttableData: [
         {
           index: 1,
@@ -124,8 +141,15 @@ export default {
           date: "2016-05-02",
           apply_condition: "",
         },
+        {
+          index: 1,
+          unicode: "12345",
+          name: "王小虎",
+          date: "2016-05-02",
+          apply_condition: "",
+        },
+        // 其他数据...
       ],
-      //完成审核列表
       secondtableData: [
         {
           index: 1,
@@ -134,37 +158,55 @@ export default {
           date: "2016-05-02",
           apply_condition: "已通过",
         },
+        {
+          index: 1,
+          unicode: "12345",
+          name: "王小虎",
+          date: "2016-05-02",
+          apply_condition: "",
+        },
+        // 其他数据...
       ],
-      currentPage1: 4,
-      currentPage2: 4,
+      currentPage1: 1,
+      currentPage2: 1,
+      pageSize1: 1,
+      pageSize2: 1,
     };
   },
-  //标签页跳转方法
   methods: {
     handleClick(tab, event) {
       console.log(tab, event);
     },
-    //同意入驻按钮,这里的两个参数
     handleSuccess(index, row) {
       console.log(index, row);
     },
-    //拒绝入驻按钮
     handleCancel(index, row) {
       console.log(index, row);
     },
     handleSendBack(index, row) {
       console.log(index, row);
     },
-    //查看企业详情按钮
     checkDetail(index, unicode) {
       console.log(index, unicode);
     },
-    //设置分页的方法
-    handleSizeChange(val) {
-      console.log(`每页 ${val} 条`);
+    handleSizeChange(pageSize) {
+      if (this.activeName === "first") {
+        this.pageSize1 = pageSize;
+      } else if (this.activeName === "second") {
+        this.pageSize2 = pageSize;
+      }
     },
-    handleCurrentChange(val) {
-      console.log(`当前页: ${val}`);
+    handleCurrentChange(currentPage) {
+      if (this.activeName === "first") {
+        this.currentPage1 = currentPage;
+      } else if (this.activeName === "second") {
+        this.currentPage2 = currentPage;
+      }
+    },
+    getCurrentPageData(data, currentPage, pageSize) {
+      const start = (currentPage - 1) * pageSize;
+      const end = start + pageSize;
+      return data.slice(start, end);
     },
   },
 };
--- src/views/declar_approval/out_approval/index.vue
+++ src/views/declar_approval/out_approval/index.vue
@@ -54,10 +54,10 @@
             @size-change="handleSizeChange"
             @current-change="handleCurrentChange"
             :current-page="currentPage1"
-            :page-sizes="[100, 200, 300, 400]"
-            :page-size="100"
+            :page-sizes="[1, 20, 50, 100]"
+            :page-size="pageSize1"
             layout="total, sizes, prev, pager, next, jumper"
-            :total="400"
+            :total="firsttableData.length"
           >
           </el-pagination>
         </div>
@@ -98,10 +98,10 @@
             @size-change="handleSizeChange"
             @current-change="handleCurrentChange"
             :current-page="currentPage2"
-            :page-sizes="[100, 200, 300, 400]"
-            :page-size="100"
+            :page-sizes="[1, 20, 50, 100]"
+            :page-size="pageSize2"
             layout="total, sizes, prev, pager, next, jumper"
-            :total="400"
+            :total="secondtableData.length"
           >
           </el-pagination>
         </div>
@@ -114,18 +114,11 @@
 export default {
   data() {
     return {
-      //标签页跳转数据
       activeName: "second",
-      // 未完成审核列表
       firsttableData: [
-        {
-          index: 1,
-          unicode: "12345",
-          name: "王小虎",
-          date: "2016-05-02",
-        },
+        { index: 1, unicode: "12345", name: "王小虎", date: "2016-05-02" },
+        // Add more data as needed
       ],
-      //完成审核列表
       secondtableData: [
         {
           index: 1,
@@ -134,33 +127,64 @@ export default {
           date: "2016-05-02",
           apply_condition: "同意搬离",
         },
+        {
+          index: 1,
+          unicode: "12345",
+          name: "王小虎",
+          date: "2016-05-02",
+          apply_condition: "同意搬离",
+        },
+        {
+          index: 1,
+          unicode: "12345",
+          name: "王小虎",
+          date: "2016-05-02",
+          apply_condition: "同意搬离",
+        },
+        {
+          index: 1,
+          unicode: "12345",
+          name: "王小虎",
+          date: "2016-05-02",
+          apply_condition: "同意搬离",
+        },
+        // Add more data as needed
       ],
-      currentPage1: 4,
-      currentPage2: 4,
+      currentPage1: 1,
+      currentPage2: 1,
+      pageSize1: 1,
+      pageSize2: 1,
     };
   },
-  //标签页跳转方法
   methods: {
     handleClick(tab, event) {
       console.log(tab, event);
     },
-    //同意搬离按钮,这里的两个参数
     handleSuccess(index, row) {
       console.log(index, row);
     },
-    //拒绝搬离按钮
     handleCancel(index, row) {
+      console.log(index, row);
+    },
+    handleSendBack(index, row) {
       console.log(index, row);
     },
     checkDetail(index, unicode) {
       console.log(index, unicode);
     },
-    //设置分页的方法
-    handleSizeChange(val) {
-      console.log(`每页 ${val} 条`);
+    handleSizeChange(pageSize) {
+      if (this.activeName === "first") {
+        this.pageSize1 = pageSize;
+      } else if (this.activeName === "second") {
+        this.pageSize2 = pageSize;
+      }
     },
-    handleCurrentChange(val) {
-      console.log(`当前页: ${val}`);
+    handleCurrentChange(currentPage) {
+      if (this.activeName === "first") {
+        this.currentPage1 = currentPage;
+      } else if (this.activeName === "second") {
+        this.currentPage2 = currentPage;
+      }
     },
   },
 };
